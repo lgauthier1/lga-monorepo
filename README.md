92cbedc359e3f1c04409b199ce55669af4542f7c
Laurent <gauthier.lrnt@gmail.com>
2024-03-03 17:37:19 +0100

    feat: initial setup wip

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+
 # lga-monorepo
+# Vue 3 + TypeScript + Vite
+
+
+## Settings
+Block install if not good engine version
+.npmrc
+```shell
+engine-strict=true 
+```
---
+++ jest.config.js
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom/jest-globals'
+
+const config = {
+  verbose: false,
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+export default config
