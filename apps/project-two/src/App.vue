bf3881d7f02c5aa065ebedc27fa18b92aa4bf321
Laurent <gauthier.lrnt@gmail.com>
2024-03-24 12:40:58 +0100

    feat: monorepo with 2 project

--- apps/project-two/src/App.vue
+++ apps/project-two/src/App.vue
@@ -11,6 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+  <h1> Projet Two</h1>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
