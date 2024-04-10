a82650faef69d642f5abe25e0404ec6a6692b60b
Laurent <gauthier.lrnt@gmail.com>
2024-04-10 15:51:44 +0200

    fix: wip

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom/jest-globals'
-
-const config = {
-  verbose: false,
-  preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom',
-}
-
-export default config
